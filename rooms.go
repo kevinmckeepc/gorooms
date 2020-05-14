ade61d990c77a3eea0f96b53ccfdc5337f822a8b
Kevin McKee <kevin.mckee@procore.com>
2020-05-14 10:39:43 -0700

    Initial commit

---
+++ rooms.go
@@ -0,0 +1,48 @@
+package rooms
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func main() {
+
+  // Local MongoDB
+  dbURI := "mongodb://localhost:27017/"
+
+  // 1) Create MongoDB client
+	client, err := mongo.NewClient(options.Client().ApplyURI(dbURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+  // 2) Connect to MongoDB
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+  // 3) List Databases
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
+}
+
+func Hello() string {
+    return "Hello, world."
+}
