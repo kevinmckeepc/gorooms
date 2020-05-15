ea9626fa04159742121cbc1f2b42dbdf1a6fedd1
Kevin McKee <kevin.mckee@procore.com>
2020-05-15 08:55:48 -0700

    The 2006 WWW is ready

--- http/server.go
+++ http/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
   "net/http"
 	"html/template"
+	"encoding/json"
 )
 
 type PageData struct {
@@ -12,28 +13,46 @@ type PageData struct {
 }
 
 var visitorCounter int
+var tmpl = template.Must(template.ParseFiles("web/templates/index.html"))
 
 func init() {
   initRoutes()
 }
 
 func initRoutes() {
+  http.HandleFunc("/", indexRoute)
+	http.HandleFunc("/data", dataRoute)
+}
 
-	tmpl := template.Must(template.ParseFiles("web/templates/index.html"))
+/// Returns the index.html
+func indexRoute(w http.ResponseWriter, r *http.Request) {
+	// Don't count the /favicon.ico request
+	if r.URL.String() == "/" {
+		visitorCounter++
+	}
 
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	data := PageData {
+		VisitorCount: visitorCounter,
+	}
 
-		// Don't count the /favicon.ico request
-		if r.URL.String() == "/" {
-			visitorCounter++
-		}
+	tmpl.Execute(w, data) // Merge the template & data
+}
 
-		data := PageData {
-			VisitorCount: visitorCounter,
-		}
+// Returns the raw visitor data in json
+func dataRoute(w http.ResponseWriter, r *http.Request) {
 
-		tmpl.Execute(w, data) // Merge the template & data
-	})
+	data := PageData {
+		VisitorCount: visitorCounter,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
 func Start() {
