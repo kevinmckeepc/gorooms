a4e7dd98be58204ddcb3bf07b2a5ac1f79f6f89a
Kevin McKee <kevin.mckee@procore.com>
2020-05-16 13:59:16 -0700

    Saving work

--- http/server.go
+++ http/server.go
@@ -12,16 +12,22 @@ type PageData struct {
 	VisitorCount int
 }
 
-var visitorCounter int
-var tmpl = template.Must(template.ParseFiles("web/templates/index.html"))
+var (
+	visitorCounter int
+	tmpl = template.Must(template.ParseFiles("web/templates/index.html"))
+	hub = newHub()
+)
 
 func init() {
   initRoutes()
+	// Run the hub in a new goroutine
+	go hub.run()
 }
 
 func initRoutes() {
   http.HandleFunc("/", indexRoute)
 	http.HandleFunc("/data", dataRoute)
+	http.HandleFunc("/ws", wsRoute)
 }
 
 /// Returns the index.html
@@ -47,6 +53,26 @@ func dataRoute(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
+// serveWs handles websocket requests from the peer.
+func wsRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Upgrading connection ...")
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("💩", err)
+		return
+	}
+
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+
+	// Allow collection of memory referenced by the caller by doing all work in
+	// new goroutines.
+	go client.writePump()
+	go client.readPump()
+	// go client.echo()
+}
+
+
 // Builds the page data to push into responses
 func data() PageData {
 	data := PageData {
@@ -56,6 +82,6 @@ func data() PageData {
 }
 
 func Start() {
-  fmt.Println("Starting http server")
+  fmt.Println("Starting http server ...")
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
